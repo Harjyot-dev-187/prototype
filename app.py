7fbd05ad9b1fdd1c5fda6a5503b504c8ec7e2952
Harjyot-dev-187 <singhharjyot321@gmail.com>
2024-10-16 20:37:38 +0530

    upload html

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return 
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
